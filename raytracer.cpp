9d1fcfeba13176e32dca2832fc404b3a1ec579a4
diogo7matias <diogoafonso.matias@gmail.com>
2025-03-31 19:03:30 +0100

    sphere class + lerp function

--- raytracer.cpp
+++ raytracer.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <cassert>
 
+#define MAX_RAY_DEPTH 5; // max recursion depth allowed
+
 // 3D Vector class
 template<typename T> class Vec3 {
 public:
@@ -27,6 +29,8 @@ public:
         return *this;
     }
 
+    // dot product
+    T dot(const Vec3<T> &v) const {return x * v.x + y * v.y + z * v.z;}
     // operator overloads
     Vec3<T> operator + (const Vec3<T> &v) const {return Vec3<T>(x + v.x, y + v.y, z + v.z);}
     Vec3<T> operator - (const Vec3<T> &v) const {return Vec3<T>(x - v.x, y - v.y, z - v.z);}
@@ -35,11 +39,53 @@ public:
     Vec3<T>& operator += (const Vec3<T> &v) {x += v.x, y += v.y, z += v.z; return *this;}
     Vec3<T>& operator *= (const Vec3<T> &v) {x *= v.x, y *= v.y, z *= v.z; return *this;}
     Vec3<T> operator - () const {return Vec3<T>(-x, -y, -z);}
-    // dot product
-    T dot(const Vec3<T> &v) const {return x * v.x + y * v.y + z * v.z;}
 
     std::ostream& operator << (std::ostream &os, const Vec3<T> &v){
         os << "[" << v.x << " " << v.y << " " << v.z << "]";
         return os;
     }
 };
+
+typedef Vec3<float> Vec3f;
+
+class Sphere {
+public:
+    Vec3f center;
+    float radius;
+    Vec3f surfaceColor, emissionColor;
+    float transparency, reflection;
+
+    // constructor
+    Sphere(
+        const Vec3f &c,
+        const float &r,
+        const Vec3f &sc,
+        const float &transp = 0,
+        const float &refl = 0,
+        const Vec3f &ec = 0
+    ) : center(c), radius(r), surfaceColor(sc), emissionColor(ec), transparency(transp), reflection(refl) {}
+
+    // checks if a ray with origin 'rayOrig' and direction 'rayDir' intersects the sphere
+    // the points in the ray where it enters and leaves the sphere are stored in t0 and t1, respectively
+    bool intersect(const Vec3f &rayOrig, const Vec3f &rayDir, float &t0, float &t1){
+        Vec3f l = center - rayOrig;
+        float tca = l.dot(rayDir); // distance given by projecting l onto ray
+        if (tca < 0) return false; // ray is pointing away from the sphere
+        float d2 = l.dot(l) - tca * tca; // perpendicular distance from center to ray squared
+        if (d2 > radius * radius) return false; // ray is completely outside the sphere
+        float thc = sqrt(radius * radius - d2); // half of the part of the ray that passes through the sphere
+        // now we find the intersections 
+        t0 = tca - thc; 
+        t1 = tca + thc;
+        return true;
+    }
+};
+
+float lerp(const float &a, const float &b, const float &mix){
+    return b * mix + a * (1 - mix);
+}
+
+// Checks if a given ray intersects any objects in the scene and shades the intersection points accordingly
+Vec3f trace(const Vec3f &rayOrig, const Vec3f &rayDir, const std::vector<Sphere> &spheres, const int &depth){
+    // NOT IMPLEMENTED
+}
