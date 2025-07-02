057d9159eaf7a6139553102b7623ae5415f396c5
diogo7matias <diogoafonso.matias@gmail.com>
2025-07-02 16:18:39 +0100

    rendering triangles

--- 2D-renderer/renderer.cpp
+++ 2D-renderer/renderer.cpp
@@ -7,13 +7,13 @@
 #include "tgaimage.h"
 #include "cmath"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
-const TGAColor blue = TGAColor(0, 0, 255, 255);
+constexpr TGAColor white = {255, 255, 255, 255};
+constexpr TGAColor red = {0, 0, 255, 255};
+constexpr TGAColor green = {0, 255, 0, 255};
+constexpr TGAColor blue = {255, 0, 0, 255};
 
-int width = 1200;
-int height = 1200;
+int width = 1300;
+int height = 1300;
 
 class Point{
      // A point (or vector) in 3D space
@@ -60,6 +60,34 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
     }
 }
 
+double signed_triangle_area(int x0, int y0, int x1, int y1, int x2, int y2){
+    return ((y1 - y0) * (x1 + x0) + (y2 - y1) * (x2 + x1) + (y0 - y2) * (x0 + x2)) / 2;
+}
+
+void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color){
+    int xmin = std::min(std::min(x0, x1), x2);
+    int ymin = std::min(std::min(y0, y1), y2);
+    int xmax = std::max(std::max(x0, x1), x2);
+    int ymax = std::max(std::max(y0, y1), y2);
+    double total_area = signed_triangle_area(x0, y0, x1, y1, x2, y2);
+    if (total_area < 1) return;
+
+    // each pixel is drawn separately so we can take advantage of parallelism
+    #pragma omp parallel for
+    for (int x = xmin; x <= xmax; x++){
+        for (int y = ymin; y <= ymax; y++){
+            // these are the barycentric coordinates of the point (x, y)
+            double alpha = signed_triangle_area(x, y, x1, y1, x2, y2) / total_area;
+            double beta = signed_triangle_area(x, y, x2, y2, x0, y0) / total_area;
+            double gamma = signed_triangle_area(x, y, x0, y0, x1, y1) / total_area;
+
+            // check if the point falls outside the triangle
+            if (alpha < 0 || beta < 0 || gamma < 0) continue;
+            image.set(x, y, color);
+        }
+    }
+}
+
 std::tuple<int, int> projection(Point p){
     return {(p.x + 1.) * width/2, (p.y + 1.) * height/2};
 }
@@ -94,10 +122,10 @@ void renderModel(std::string filename, TGAImage &image){
             auto [bx, by] = projection(points[stoi(components[2]) - 1]);
             auto [cx, cy] = projection(points[stoi(components[3]) - 1]);
 
-            // draw lines for every pair of verticess
-            line(ax, ay, bx, by, image, green);
-            line(bx, by, cx, cy, image, green);
-            line(cx, cy, ax, ay, image, green);
+            // render triangles with random colors
+            TGAColor randColor;
+            for (int c = 0; c < 3; c++) randColor[c] = std::rand() % 255;
+            triangle(ax, ay, bx, by, cx, cy, image, randColor);
             // draw points
             image.set(ax, ay, white);
             image.set(bx, by, white);
@@ -116,7 +144,12 @@ int main(int argc, char const *argv[]){
     }
 
     renderModel(argv[1], image);
-	image.flip_vertically();
+
+    // triangle(7, 45, 35, 100, 45, 60, image, red);
+    // triangle(120, 35, 90, 5, 45, 110, image, white);
+    // triangle(115, 83, 80, 90, 85, 120, image, green);
+
+	// image.flip_vertically();
     image.write_tga_file("output.tga");
     return 0;
 }
