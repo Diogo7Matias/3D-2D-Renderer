b537844031fcb8b834ee9cb6a7c0d3d90a65acd2
diogo7matias <diogoafonso.matias@gmail.com>
2025-07-02 10:22:25 +0100

    rendering model from obj file

--- 2D-renderer/renderer.cpp
+++ 2D-renderer/renderer.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "tgaimage.h"
 #include "cmath"
 
@@ -5,6 +11,21 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
+
+int width = 1200;
+int height = 1200;
+
+class Point{
+     // A point (or vector) in 3D space
+public:
+    float x, y, z;
+    
+    Point(float xCoord, float yCoord, float zCoord){
+        x = xCoord;
+        y = yCoord;
+        z = zCoord;
+    }
+};
 
 // this function implements the Bresenham's line drawing algorithm
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
@@ -39,25 +60,62 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
     }
 }
 
+std::tuple<int, int> projection(Point p){
+    return {(p.x + 1.) * width/2, (p.y + 1.) * height/2};
+}
+
+void renderModel(std::string filename, TGAImage &image){
+    std::ifstream objFile;
+    std::string fline;
+    std::vector<Point> points;
+    
+    objFile.open(filename);
+    while (std::getline(objFile, fline)){
+        std::istringstream iss(fline);
+        std::vector<std::string> components;
+        std::string token;
+
+        // split line by whitespace
+        while (iss >> token){
+            std::istringstream tokenStream(token);
+            std::string component;
+            
+            std::getline(tokenStream, component, '/');
+            components.push_back(component);
+        }
+
+        if (!components.size()) continue;
+
+        if (components[0] == "v") { // parse a point
+            Point point(stof(components[1]), stof(components[2]), stof(components[3]));
+            points.push_back(point);
+        } else if (components[0] == "f") { // parse a face (3 vertices)
+            auto [ax, ay] = projection(points[stoi(components[1]) - 1]);
+            auto [bx, by] = projection(points[stoi(components[2]) - 1]);
+            auto [cx, cy] = projection(points[stoi(components[3]) - 1]);
+
+            // draw lines for every pair of verticess
+            line(ax, ay, bx, by, image, green);
+            line(bx, by, cx, cy, image, green);
+            line(cx, cy, ax, ay, image, green);
+            // draw points
+            image.set(ax, ay, white);
+            image.set(bx, by, white);
+            image.set(cx, cy, white);
+        }
+    }
+    objFile.close();
+}
+
 int main(int argc, char const *argv[]){
-	TGAImage image(64, 64, TGAImage::RGB);
-    // line(13, 20, 80, 40, image, white);
-    // line(13, 20, 5, 80, image, white);
+	TGAImage image(width, height, TGAImage::RGB);
 
-    int ax = 7, ay = 3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53; 
-    
-    // lines
-    line(ax, ay, bx, by, image, blue);
-    line(cx, cy, bx, by, image, green);
-    line(ax, ay, cx, cy, image, red);
-    
-    // points
-    image.set(ax, ay, white);
-    image.set(bx, by, white);
-    image.set(cx, cy, white);
+    if (argc != 2){
+        std::cout << "Usage: " << argv[0] << " objmodel.obj" << std::endl;
+        return 1;
+    }
 
+    renderModel(argv[1], image);
 	image.flip_vertically();
     image.write_tga_file("output.tga");
     return 0;
