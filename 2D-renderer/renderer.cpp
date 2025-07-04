793afaae8df770c3c0466b026133fb9562b8cf07
diogo7matias <diogoafonso.matias@gmail.com>
2025-07-04 16:57:21 +0100

    zbuffer

--- 2D-renderer/renderer.cpp
+++ 2D-renderer/renderer.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cassert>
 
 #include "tgaimage.h"
 #include "cmath"
@@ -15,17 +16,38 @@ constexpr TGAColor blue = {255, 0, 0, 255};
 int width = 1300;
 int height = 1300;
 
-class Point{
-     // A point (or vector) in 3D space
+
+template<int n> class Vec {
+    double coords[n] = {0};
 public:
-    float x, y, z;
-    
-    Point(float xCoord, float yCoord, float zCoord){
-        x = xCoord;
-        y = yCoord;
-        z = zCoord;
+    Vec() = default;
+
+    Vec(std::initializer_list<double> list){
+        assert(list.size() <= n);
+        std::copy(list.begin(), list.end(), coords);
+    }
+
+    template<typename Tuple, std::size_t... Is>
+    Vec(const Tuple& t, std::index_sequence<Is...>){
+        ((coords[Is] = std::get<Is>(t)), ...);
+    }
+
+    template<typename Tuple>
+    Vec(const Tuple t) : Vec(t, std::make_index_sequence<std::tuple_size<Tuple>::value>{}) {}
+
+
+    double &operator[](const int i){
+        assert (i >= 0 && i < n);
+        return coords[i];
+    }
+
+    double operator[](const int i) const{
+        assert (i >= 0 && i < n);
+        return coords[i];
     }
 };
+
+typedef Vec<3> Vec3;
 
 // this function implements the Bresenham's line drawing algorithm
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
@@ -64,12 +86,12 @@ double signed_triangle_area(int x0, int y0, int x1, int y1, int x2, int y2){
     return ((y1 - y0) * (x1 + x0) + (y2 - y1) * (x2 + x1) + (y0 - y2) * (x0 + x2)) / 2;
 }
 
-void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color){
-    int xmin = std::min(std::min(x0, x1), x2);
-    int ymin = std::min(std::min(y0, y1), y2);
-    int xmax = std::max(std::max(x0, x1), x2);
-    int ymax = std::max(std::max(y0, y1), y2);
-    double total_area = signed_triangle_area(x0, y0, x1, y1, x2, y2);
+void triangle(Vec3 p1, Vec3 p2, Vec3 p3, TGAImage &image, TGAImage &zbuffer, TGAColor color){
+    int xmin = std::min(std::min(p1[0], p2[0]), p3[0]);
+    int ymin = std::min(std::min(p1[1], p2[1]), p3[1]);
+    int xmax = std::max(std::max(p1[0], p2[0]), p3[0]);
+    int ymax = std::max(std::max(p1[1], p2[1]), p3[1]);
+    double total_area = signed_triangle_area(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1]);
     if (total_area < 1) return;
 
     // each pixel is drawn separately so we can take advantage of parallelism
@@ -77,25 +99,34 @@ void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, T
     for (int x = xmin; x <= xmax; x++){
         for (int y = ymin; y <= ymax; y++){
             // these are the barycentric coordinates of the point (x, y)
-            double alpha = signed_triangle_area(x, y, x1, y1, x2, y2) / total_area;
-            double beta = signed_triangle_area(x, y, x2, y2, x0, y0) / total_area;
-            double gamma = signed_triangle_area(x, y, x0, y0, x1, y1) / total_area;
+            double alpha = signed_triangle_area(x, y, p2[0], p2[1], p3[0], p3[1]) / total_area;
+            double beta = signed_triangle_area(x, y, p3[0], p3[1], p1[0], p1[1]) / total_area;
+            double gamma = signed_triangle_area(x, y, p1[0], p1[1], p2[0], p2[1]) / total_area;
 
             // check if the point falls outside the triangle
             if (alpha < 0 || beta < 0 || gamma < 0) continue;
+
+            unsigned char z = alpha * p1[2] + beta * p2[2] + gamma * p3[2];
+
+            // if the pixel is behind something, dont paint it
+            if (z <= zbuffer.get(x, y)[0]) continue;
+
+            zbuffer.set(x, y, {z});
             image.set(x, y, color);
         }
     }
 }
 
-std::tuple<int, int> projection(Point p){
-    return {(p.x + 1.) * width/2, (p.y + 1.) * height/2};
+std::tuple<int, int, int> projection(Vec3 p){
+    return {(p[0] + 1.) * width/2, 
+            (p[1] + 1.) * height/2, 
+            (p[2] + 1.) * 255/2};
 }
 
-void renderModel(std::string filename, TGAImage &image){
+void renderModel(std::string filename, TGAImage &image, TGAImage &zbuffer){
     std::ifstream objFile;
     std::string fline;
-    std::vector<Point> points;
+    std::vector<Vec3> points;
     
     objFile.open(filename);
     while (std::getline(objFile, fline)){
@@ -115,21 +146,17 @@ void renderModel(std::string filename, TGAImage &image){
         if (!components.size()) continue;
 
         if (components[0] == "v") { // parse a point
-            Point point(stof(components[1]), stof(components[2]), stof(components[3]));
+            Vec3 point = {stof(components[1]), stof(components[2]), stof(components[3])};
             points.push_back(point);
         } else if (components[0] == "f") { // parse a face (3 vertices)
-            auto [ax, ay] = projection(points[stoi(components[1]) - 1]);
-            auto [bx, by] = projection(points[stoi(components[2]) - 1]);
-            auto [cx, cy] = projection(points[stoi(components[3]) - 1]);
+            Vec3 p1 = projection(points[stoi(components[1]) - 1]);
+            Vec3 p2 = projection(points[stoi(components[2]) - 1]);
+            Vec3 p3 = projection(points[stoi(components[3]) - 1]);
 
             // render triangles with random colors
             TGAColor randColor;
             for (int c = 0; c < 3; c++) randColor[c] = std::rand() % 255;
-            triangle(ax, ay, bx, by, cx, cy, image, randColor);
-            // draw points
-            image.set(ax, ay, white);
-            image.set(bx, by, white);
-            image.set(cx, cy, white);
+            triangle(p1, p2, p3, image, zbuffer, randColor);
         }
     }
     objFile.close();
@@ -137,20 +164,16 @@ void renderModel(std::string filename, TGAImage &image){
 
 int main(int argc, char const *argv[]){
 	TGAImage image(width, height, TGAImage::RGB);
+    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
     if (argc != 2){
         std::cout << "Usage: " << argv[0] << " objmodel.obj" << std::endl;
         return 1;
     }
 
-    renderModel(argv[1], image);
-
-    // triangle(7, 45, 35, 100, 45, 60, image, red);
-    // triangle(120, 35, 90, 5, 45, 110, image, white);
-    // triangle(115, 83, 80, 90, 85, 120, image, green);
-
-	// image.flip_vertically();
+    renderModel(argv[1], image, zbuffer);
     image.write_tga_file("output.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
     return 0;
 }
 
